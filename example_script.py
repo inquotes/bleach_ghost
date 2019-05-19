cb5fb32e094b700358e9ecc0f2c16cad9daffb9b
inquotes <ataitague@gmail.com>
2019-05-19 11:10:47 -0700

    fixing some spaces

--- example_script.py
+++ example_script.py
@@ -1,11 +1,11 @@
 from BleachBot import BleachBot
 
 example_bot = BleachBot('CONSUMER_KEY', 'CONSUMER_SECRET', 
-					    'ACCESS_TOKEN', 'ACCESS_SECRET')
+			'ACCESS_TOKEN', 'ACCESS_SECRET')
 
 # This script will find recent tweets with either of the terms,
 # process the text a little, train a simple model to predict the 
-# next character given, the last 7 characters in the processed
+# next character given the last 7 characters in the processed
 # text, and finally use the model to produce a 10 line poem
 
 example_bot.set_search_terms(['puppy','dog'])
